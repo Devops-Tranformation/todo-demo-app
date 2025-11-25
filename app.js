d46eb4c2f95b2d49f84c484a00069d1530d30b62
Ramthota <ramdas4987@gmail.com>
2025-11-25 23:39:39 +0530

    Test git trigger

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const todos = [
   "Learn Jenkins pipelines",
   "Automate builds with Git",
   "Deploy sample Node app",
-  "First check after push",
+  "Test Git tigger actions",
 ];
 
 app.get('/', (req, res) => {
   res.send(`
-    <h1>TODO Application by Ram</h1>
+    <h1>TODO App by Ram</h1>
     <ul>
       ${todos.map(item => `<li>${item}</li>`).join('')}
     </ul>
