3d77e91b987d80fe0adb89bbad92832b393b477b
Ram Thota <ramdas4987@gmail.com>
2025-11-25 11:54:38 +0530

    Initialize TODO application with startup logs

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("✅ TODO Application is starting...");
+console.log("📝 Current TODOs:");
+console.log("- Learn Jenkins pipelines");
+console.log("- Automate builds with Git");
+console.log("- Deploy sample Node app");
